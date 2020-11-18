76faa1eb3d8ecb4dcfaa352076548460147e0fdb
zhengzhigang <15035989282@163.com>
2020-11-18 14:21:56 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <router-view />
+    <h1>代码测试</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  created () {},
+  mounted () {},
+  methods: {}
+}
+</script>
+<style lang="less">
+</style>
